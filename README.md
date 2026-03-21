062a80b9e620c303e3f39bef2d3127977e7b6b57
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2026-03-20 22:00:26 -0700

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+This is nothing fancy. Once changes are made to the `systemd` fork -- just run the basic tests.
+That's all.
---
+++ test.sh
@@ -0,0 +1,15 @@
+# one-off runs
+# apt install debian-archive-keyring # for some reason mkosi does not like ubuntu-keyring (?)
+# git clone https://github.com/systemd/mkosi.git # pull mkosi if it's not there already
+# git clone https://github.com/Jeffrey-Sardina/systemd.git # pull the fork of systemd if it's not there already
+
+# update mkosi
+cd mkosi
+git pull
+
+# run tests (see: https://systemd.io/HACKING/)
+cd ../systemd
+../mkosi/bin/mkosi -f genkey
+../mkosi/bin/mkosi -f box -- meson setup build
+../mkosi/bin/mkosi -f box -- meson compile -C build mkosi
+../mkosi/bin/mkosi vm
