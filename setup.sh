f1ef2de2d7b8096391ffecfbd375fc31bca8c9a4
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2026-04-28 07:50:46 -0700

    update setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 # apt install debian-archive-keyring # change this to the appropriate keyring for your distro
 git clone https://github.com/systemd/mkosi.git # pull mkosi if it's not there already
 git clone https://github.com/Jeffrey-Sardina/systemd.git # pull the fork of systemd if it's not there already
+git clone https://github.com/systemd/systemd.git # pull base systemd -- we run tests here too, to see if errors observed are present upstream or unique to Liberated systemd
 cd systemd/
 git remote add upstream https://github.com/systemd/systemd.git
 
